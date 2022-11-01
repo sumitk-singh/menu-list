66ed54a1b52ce6dc031a5c21bc1554215e4e94ef
SUMIT SINGH <76554602+sumitk-singh@users.noreply.github.com>
2022-11-01 08:24:00 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    id: 1,
+    food: "egg curry",
+    price: 299
+  },
+
+  {
+    id: 2,
+    food: "Paneer paratha",
+    price: 85
+  },
+
+  {
+    id: 3,
+    food: "Rice",
+    price: 65
+  },
+
+  {
+    id: 4,
+    food: "Mutton",
+    price: 587
+  },
+
+  {
+    id: 5,
+    food: "Coffee",
+    price: 65
+  }
+];
